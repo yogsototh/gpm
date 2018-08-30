3d9f0acc577a38c9fedaa931534ed56771a17f85
Yann Esposito (Yogsototh) <yann.esposito@gmail.com>
2018-08-30 18:33:02 +0200

    Command with subcommands, init, new-issue, start-review, end-review

--- gpm.hs
+++ gpm.hs
@@ -4,11 +4,43 @@
 #! nix-shell -I nixpkgs="https://github.com/NixOS/nixpkgs/archive/16d475334409f7fa632929b2838421b4ffe34927.tar.gz"
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-import Protolude hiding (stdout,(%))
+import Protolude hiding (stdout,(%),die)
 import Turtle
 
 main :: IO ()
 main = do
+  x <- options "Git Project Manager" parser
+  case x of
+   Init -> init
+   NewIssue -> newIssue
+   StartReview br -> startReview br
+   EndReview br -> endReview br
+
+data Command = Init
+             | NewIssue
+             | StartReview (Maybe Text)
+             | EndReview (Maybe Text)
+             deriving (Eq)
+
+parser :: Parser Command
+parser = subcommand "init" "Initialize gpm" (pure Init)
+         <|> subcommand "new-issue" "Create a new Issue" (pure NewIssue)
+         <|> StartReview <$> subcommand "start-review" "Start review (use current branch by default)"
+                             (optional (argText "branch" "The git branch to review"))
+         <|> EndReview   <$> subcommand "end-review" "End review (use current branch by default)"
+                             (optional (argText "branch" "The git branch to end review"))
+
+newIssue :: IO ()
+newIssue = die "TODO"
+
+startReview :: Maybe Text -> IO ()
+startReview br = die "TODO"
+
+endReview :: Maybe Text -> IO ()
+endReview br = die "TODO"
+
+init :: IO ()
+init = do
   echo "# <GPM> -- Git Project Manager"
   mkNewEmptyBranch "gpm"
   initIssues
