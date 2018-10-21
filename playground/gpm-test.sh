189b181b132f8b8a9d0654436d637eff6ff767cf
Yann Esposito (Yogsototh) <yann.esposito@gmail.com>
2018-10-21 16:48:52 +0200

    do not forget to update the repo before cloning

--- playground/gpm-test.sh
+++ playground/gpm-test.sh
@@ -21,6 +21,7 @@ pushd $testproj
 title "CLEANING UP"
 [[ -d $testproj ]] && rm -rf $testproj
 [[ -d $testproj2 ]] && rm -rf $testproj2
+
 title "CREATE PROJECT $testproj"
 mkdir $testproj
 pushd $testproj
@@ -30,17 +31,22 @@ git add README
 git commit -m "initial commit"
 title "gpm init"
 gpm init
+
 title "gpm new-issue"
 gpm new-issue -t "issue-1" -p "A"
+
 title "HOOKS"
 git co gpm
 cp hooks/prepare-commit-msg{.sample,}
 git add hooks
 git commit -m "updated the prepare-commit-msg git hook"
 git co master
+
 title "gpm hooks sync"
 gpm hooks sync
+
 title "gpm serve start"
+gpm serve update
 gpm serve start
 popd
 mkdir $testproj2
